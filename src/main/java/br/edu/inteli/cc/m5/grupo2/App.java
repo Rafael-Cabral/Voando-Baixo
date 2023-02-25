e193280c0153ecd42d77e48fce35b4c2561f862d
Pedro Romão <pedro.dias@sou.inteli.com.br>
2023-02-25 11:03:45 -0300

    graph class test

--- src/main/java/br/edu/inteli/cc/m5/grupo2/App.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/App.java
@@ -9,8 +9,8 @@ public class App {
         String[] paths = new String[]{"src/main/resources/dted/SaoPaulo/W045_S23.dt2", "src/main/resources/dted/SaoPaulo/W045_S24.dt2", "src/main/resources/dted/SaoPaulo/W046_S23.dt2", "src/main/resources/dted/SaoPaulo/W046_S24.dt2", "src/main/resources/dted/SaoPaulo/W047_S23.dt2", "src/main/resources/dted/SaoPaulo/W047_S24.dt2"};
         double[][] map = new double[0][];
 
-        for (int i = 0; i < paths.length; i++) {
-            double[][] newMap = Dted.readDted(paths[i], 180);
+        for (String path : paths) {
+            double[][] newMap = Dted.readDted(path, 180);
             map = Dted.mergeDted(map, newMap);
         }
 
@@ -18,8 +18,8 @@ public class App {
         map = Dted.sortDted(map);
 
         // Sending all Vertices to the graph created
-        for (int i = 0; i < map.length; i++) {
-            graph.addVertex(map[i][1], map[i][2], map[i][0]);
+        for (double[] doubles : map) {
+            graph.addVertex(doubles[1], doubles[2], doubles[0]);
         }
 
         // Creating all possible connections in the graph
