9bd13d070ae4b8701eacb106bee25543b98c6cf2
Pedro Romão <pedro.dias@sou.inteli.com.br>
2023-02-25 11:41:09 -0300

    vertexTest

--- src/main/java/br/edu/inteli/cc/m5/grupo2/VertexTest.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/VertexTest.java
@@ -4,24 +4,25 @@ import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-public class EdgeTest {
+public class VertexTest {
+
     @Test
-    private void assureWeight(){
+    private void testConnection(){
         Vertex v1 = new Vertex(1, 1.0, 1.0, 100.0);
         Vertex v2 = new Vertex(2, 2.0, 2.0, 200.0);
 
         v1.addConnectionTo(v2);
-
         assertEquals(300, v1.getConnectionAt(0).getWeight());
+        assertEquals(v2, v1.getConnectionAt(0).getArrivalVertex());
     }
 
     @Test
-    private void assureArrivalVertex(){
+    private void assureAdjacencyList(){
         Vertex v1 = new Vertex(1, 1.0, 1.0, 100.0);
         Vertex v2 = new Vertex(2, 2.0, 2.0, 200.0);
 
         v1.addConnectionTo(v2);
 
-        assertEquals(v2, v1.getConnectionAt(0).getArrivalVertex());
+        assertEquals("1: 2", v1.getAdjacencyListAsString());
     }
 }
