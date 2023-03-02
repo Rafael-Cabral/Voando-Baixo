17b30542fe20760441507f9aea31a58ee3c2f905
Elias Biondo <contato@eliasbiondo.com>
2023-03-02 10:02:24 -0300

    feat: add api health

---
+++ src/main/back-end/src/index.ts
@@ -0,0 +1,27 @@
+import { Application } from "express";
+import App from "./app";
+import HttpServer from "./httpServer";
+
+class Setup {
+
+	public app : App;
+
+	constructor() {
+
+		this.app = new App();
+
+		const { app } = this.app;
+		
+		this.up(app);
+
+	}
+
+	private up(app : Application) {
+
+		new HttpServer(app, 3000);
+
+	}
+
+}
+
+export default new Setup();
