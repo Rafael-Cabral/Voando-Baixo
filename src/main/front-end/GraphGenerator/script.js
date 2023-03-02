827b7af882668e8ad89632976b4ffb0ff3a74478
BartonThomas <99219127+BartonThomas@users.noreply.github.com>
2023-03-02 11:43:58 -0300

    Refactor: script.js
    Changed node colors to black and added numbers in white to them. Also small changes in styles.css and main.html

--- src/main/front-end/GraphGenerator/script.js
+++ src/main/front-end/GraphGenerator/script.js
@@ -21,46 +21,51 @@ function createPath(){
     generateGraph();
 }
 
-function generateGraph(){
-    var svg = d3.select("div#visualization")
+function generateGraph() {
+  var svg = d3.select("div#visualization")
     .append("svg")
     .attr("width", 500)
     .attr("height", 500);
 
-    // Define the force simulation
-    var simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(function(d) { return d.index; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(250, 250));
+  // Define the force simulation
+  var simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(function(d) { return d.index; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(250, 250));
 
-    // Add links to the visualization
-    var link = svg.selectAll(".link")
+  // Add links to the visualization
+  var link = svg.selectAll(".link")
     .data(links)
     .enter()
     .append("line")
     .attr("class", "link");
 
-    // Add nodes to the visualization
-    var node = svg.selectAll(".node")
-  .data(nodes)
-  .enter()
-  .append("circle")
-  .attr("class", "node")
-  .attr("r", 10)
-  .attr("fill", function(d, i) {
-    if (i == 0) {return "green";}
-    else if (i == nodes.length - 1) {return "yellow";}
-    else {return "blue";}
-  });
+  // Add nodes to the visualization
+  var node = svg.selectAll(".node")
+    .data(nodes)
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-    // Define the node position and link endpoints
-    simulation.on("tick", function() {
+  node.append("circle")
+    .attr("r", 10)
+    .attr("fill", "black");
+
+  // Append text to each node
+  node.append("text")
+    .attr("text-anchor", "middle")
+    .attr("dy", ".35em")
+    .attr("fill", "white")
+    .text(function(d) { return d.altitude; });
+
+  // Define the node position and link endpoints
+  simulation.on("tick", function() {
     link.attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
-    node.attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-    });
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
 }
