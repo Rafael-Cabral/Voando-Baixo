c3ae541f6285d152af5ce9543daf7f5e094ed200
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2023-03-06 16:01:58 -0300

    Bug: findPath

--- src/main/java/br/edu/inteli/cc/m5/grupo2/App.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/App.java
@@ -1,30 +1,35 @@
 package br.edu.inteli.cc.m5.grupo2;
 
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) {
-
-        // Instancing a new graph.
         Graph graph = new Graph();
 
-        // path to DTED file
-        String path = "src/main/resources/dted/SaoPaulo/W045_S23.dt2";
+        Vertex v1 = new Vertex(1, 0, 0, 0);
+        Vertex v2 = new Vertex(2, 0, 0, 10);
+        Vertex v3 = new Vertex(3, 0, 0, 20);
+        Vertex v4 = new Vertex(4, 0, 0, 30);
+        Vertex v5 = new Vertex(5, 0, 0, 40);
 
-        double[][] map = Dted.readDted(path, 180);
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+        graph.addVertex(v5);
 
-        // Sending all Vertices to the graph created
-        for (int i = 0;i < map.length - 1; i++) {
-            graph.addVertex(map[i][1], map[i][2], map[i][0]);
-        }
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
 
-        int rows = (int) map[map.length - 1][1];
-        int cols = (int) map[map.length - 1][2];
+        v1.addConnectionTo(v2);
+        v2.addConnectionTo(v3);
+        v3.addConnectionTo(v4);
+        v4.addConnectionTo(v5);
+        v1.addConnectionTo(v5);
 
-        // Creating all possible connections in the graph
-        graph.connectVertices(180, rows, cols);
-
-        // Printing the connections of id = 6 vertex.
-        graph.getConnectionsOf(19000).forEach(connection -> {
-            System.out.println(connection.getArrivalVertex().getId());
-        });
+        LinkedList<Vertex> path = graph.findPath(v1, v5);
+        System.out.println(path);
     }
 }
