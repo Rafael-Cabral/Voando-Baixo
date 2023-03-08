835267a7fb959a3b9b0b9b24ce9f5891d507dea6
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2023-03-08 07:40:51 -0300

    Enhancement: AStar

--- src/main/java/br/edu/inteli/cc/m5/grupo2/AStar.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/AStar.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class AStar {
-    public double Heuristica(Vertex start, Vertex end) {
+    public static double heuristica(Vertex start, Vertex end) {
         double d1 = Math.abs(end.getLatitude() - start.getLatitude());
         double d2 = Math.abs(end.getLongitude() - start.getLongitude());
         return Math.sqrt(Math.pow(d1, 2) + Math.pow(d2, 2));
@@ -24,6 +24,8 @@ public class AStar {
         start.setCustoDoInicio(0);
 
         //Inicia o custo estimado total
+        start.setCustoEstimadoTotal(heuristica(start, end));
+
         //Condição que verifica, passo a passo qual o vértice mais barato
         //Condição que cria o caminho
 
