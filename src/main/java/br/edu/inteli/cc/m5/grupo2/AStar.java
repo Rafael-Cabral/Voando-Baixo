44cf7a5624528f1be6b886b76c8f89b7124c7ab6
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2023-03-08 07:24:07 -0300

    Bug: findPath

--- src/main/java/br/edu/inteli/cc/m5/grupo2/AStar.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/AStar.java
@@ -1,9 +1,18 @@
 package br.edu.inteli.cc.m5.grupo2;
 
+import java.util.List;
+
 public class AStar {
     public double Heuristica(Vertex start, Vertex end) {
         double d1 = Math.abs(end.getLatitude() - start.getLatitude());
         double d2 = Math.abs(end.getLongitude() - start.getLongitude());
         return Math.sqrt(Math.pow(d1, 2) + Math.pow(d2, 2));
     }
+
+    public static List<Vertex> findPath(Vertex start, Vertex end) {
+
+
+
+    }
+
 }
