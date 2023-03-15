a7e24cf896b2926bdbb6b5c4c6739dce98ec7e1f
Elias Biondo <contato@eliasbiondo.com>
2023-03-15 19:08:37 -0300

    fix: font family

--- src/main/front-end/altitudeprocc/src/Global.style.tsx
+++ src/main/front-end/altitudeprocc/src/Global.style.tsx
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
-    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
-
     :root {
         font-size: 62.5%;
     }
@@ -16,6 +14,12 @@ export const GlobalStyle = createGlobalStyle`
         outline: none;
         box-sizing: border-box;
         font-family: "Inter", sans-serif;
+
+    }
+
+    body {
+
+        background-color: #F8F8FA;
 
     }
 
