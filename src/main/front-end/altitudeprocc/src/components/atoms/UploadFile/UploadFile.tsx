949387ff3ea8fcc41e598916cf3ef1463f98a915
Elias Biondo <contato@eliasbiondo.com>
2023-03-16 17:55:30 -0300

    feat: file upload

---
+++ src/main/front-end/altitudeprocc/src/components/atoms/UploadFile/UploadFile.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text } from '../Text/Text';
+import { UploadInput, UploadWrapper } from './UploadFile.style';
+
+export const UploadFile = ({mb, mt, ml, mr}: React.PropsWithChildren<any>) => {
+
+    const [fileInput, setFileInput] = React.useState<File>();
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            setFileInput(event.target.files[0]);
+        } else {
+            setFileInput(undefined);
+        }
+    }
+
+    return (
+        <UploadWrapper mb={mb} mt={mt} ml={ml} mr={mr} htmlFor="file">
+
+            {!fileInput ? ( 
+                <>
+                    <Text size="small" weight="bold" color="#919299" align="center">Dados de elevação do terreno</Text>
+                    <Text size="small" weight="regular" color="#919299" maxWidth="24rem" align="center">clique para navegar até o arquivo <br/>(apenas .dt2)</Text>
+                </>
+            ) : (
+                    <Text size="small" weight="bold" color="#919299" align="center"> {fileInput.name}</Text>
+            )}
+
+            <UploadInput type="file" id="file" accept=".dt2" onChange={handleFileChange}/>
+
+        </UploadWrapper>
+    );
+}
--- src/main/front-end/altitudeprocc/src/components/molecules/CreateProjectPopup/CreateProjectPopup.tsx
+++ src/main/front-end/altitudeprocc/src/components/molecules/CreateProjectPopup/CreateProjectPopup.tsx
@@ -4,6 +4,7 @@ import { Input } from "../../atoms/Input/Input";
 import { Text } from "../../atoms/Text/Text";
 import { StyledCreateProjectPopup } from "./CreateProjectPopup.styles"
 import { ReactComponent as Folder } from "../../../assets/folder.svg";
+import { UploadFile } from "../../atoms/UploadFile/UploadFile";
 
 interface CreateProjectPopupProps {
     closePopup: any;
@@ -14,7 +15,8 @@ export const CreateProjectPopup = ({closePopup} : React.PropsWithChildren<Create
     return (
         <StyledCreateProjectPopup>
             <Text size="large" weight="semi" mb="3.2rem">Criar novo projeto</Text>
-            <Input type="text" icon={<Folder />} placeholder="Nome do projeto" value="" mb="3.2rem"></Input>
+            <Input type="text" icon={<Folder />} placeholder="Nome do projeto" value="" mb="1.6rem"></Input>
+            <UploadFile mb="3.2rem"/>
             <Button type="button" variant="primary" mb="1.6rem">Criar projeto</Button>
             <Button type="button" variant="secondary" onClick={() => {closePopup(false)}}>Cancelar</Button>
         </StyledCreateProjectPopup>
