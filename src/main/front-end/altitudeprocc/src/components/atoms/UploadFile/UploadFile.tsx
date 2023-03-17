d98684a24d0bf889db161aaff2f67f6fd9eac834
Elias Biondo <contato@eliasbiondo.com>
2023-03-17 18:01:02 -0300

    feat: file upload

--- src/main/front-end/altitudeprocc/src/components/atoms/UploadFile/UploadFile.tsx
+++ src/main/front-end/altitudeprocc/src/components/atoms/UploadFile/UploadFile.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { Text } from '../Text/Text';
 import { UploadInput, UploadWrapper } from './UploadFile.style';
+import axios from 'axios';
 
-export const UploadFile = ({mb, mt, ml, mr, fileInput, setFileInput}: React.PropsWithChildren<any>) => {
+export const UploadFile = ({mb, mt, ml, mr, fileInput, setFileInput, uploadStatus, setUploadStatus, uploadedFileData, setUploadedFileData}: React.PropsWithChildren<any>) => {
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+
         if (event.target.files && event.target.files.length > 0) {
+
             setFileInput(event.target.files[0]);
+
         } else {
+
             setFileInput(undefined);
+
         }
+
     }
+
+    const handleFileUpload = async () => {
+
+        const formData = new FormData();
+
+        formData.append('file', fileInput);
+
+        const response = await axios.post('http://localhost:3000/api/uploads', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: (progressEvent) => {
+                const { loaded, total } = progressEvent;
+                let percent = Math.floor((loaded * 100) / (total || 1));
+                if (percent < 100) {
+                    setUploadStatus(`Upload em andamento: ${percent}%`);
+                } else {
+                    setUploadStatus(`Upload concluído!`);
+                }
+            }
+        });
+
+        setUploadedFileData(response.data?.success?.data);
+        
+    }
+
+    useEffect(() => {
+        if(fileInput) {
+            handleFileUpload();
+        }
+    }, [fileInput])
 
     return (
         <UploadWrapper mb={mb} mt={mt} ml={ml} mr={mr} htmlFor="file">
@@ -22,7 +60,10 @@ export const UploadFile = ({mb, mt, ml, mr, fileInput, setFileInput}: React.Prop
                     <Text size="small" weight="regular" color="#919299" maxWidth="24rem" align="center">clique para navegar até o arquivo <br/>(apenas .dt2)</Text>
                 </>
             ) : (
+                <>
                     <Text size="small" weight="bold" color="#919299" align="center"> {fileInput.name}</Text>
+                    <Text size="small" weight="regular" color="#919299" maxWidth="24rem" align="center">{uploadStatus}</Text>
+                </>
             )}
 
             <UploadInput type="file" id="file" accept=".dt2" onChange={handleFileChange}/>
--- src/main/front-end/altitudeprocc/src/components/molecules/CreateProjectPopup/CreateProjectPopup.tsx
+++ src/main/front-end/altitudeprocc/src/components/molecules/CreateProjectPopup/CreateProjectPopup.tsx
@@ -5,6 +5,9 @@ import { Text } from "../../atoms/Text/Text";
 import { StyledCreateProjectPopup } from "./CreateProjectPopup.styles"
 import { ReactComponent as Folder } from "../../../assets/folder.svg";
 import { UploadFile } from "../../atoms/UploadFile/UploadFile";
+import axios from "axios";
+import { Notyf } from "notyf";
+import 'notyf/notyf.min.css';
 
 interface CreateProjectPopupProps {
     closePopup: any;
@@ -13,23 +16,56 @@ interface CreateProjectPopupProps {
 export const CreateProjectPopup = ({closePopup} : React.PropsWithChildren<CreateProjectPopupProps>) => {
 
     const [fileInput, setFileInput] = React.useState<File>();
+    const [uploadStatus, setUploadStatus] = React.useState<string>("Upload em andamento: 0%");
+    const [uploadedFileData, setUploadedFileData] = useState<any>({});
     const [projectName, setProjectName] = useState<string>("");
     const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
 
     useEffect(() => {
-        if (fileInput && projectName) {
+        if (fileInput && projectName.length > 0 && uploadStatus === "Upload concluído!" && uploadedFileData) {
             setIsButtonDisabled(false);
         } else {
             setIsButtonDisabled(true);
         }
-    }, [fileInput, projectName]);
+    }, [fileInput, projectName, uploadStatus, uploadedFileData]);
+
+    const handleCreateProject = async () => {
+
+        const json = JSON.stringify({
+            project: {
+                name: projectName,
+                objectKey: uploadedFileData.objectKey,
+            }
+        });
+
+        const notyf = new Notyf();
+
+        try {
+
+            await axios.post('http://localhost:3000/api/projects', json, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            notyf.success("Projeto criado com sucesso!");
+
+        } catch (error) {
+
+            notyf.error("Erro ao criar projeto!");
+
+        }
+
+        closePopup(false)
+
+    }
 
     return (
         <StyledCreateProjectPopup>
             <Text size="large" weight="semi" mb="3.2rem">Criar novo projeto</Text>
             <Input type="text" icon={<Folder />} placeholder="Nome do projeto" value={projectName} mb="1.6rem" onChange={(event) => {setProjectName(event.target.value)}}></Input>
-            <UploadFile mb="3.2rem" fileInput={fileInput} setFileInput={setFileInput}/>
-            <Button type="button" variant="primary" mb="1.6rem" disabled={isButtonDisabled}>Criar projeto</Button>
+            <UploadFile mb="3.2rem" fileInput={fileInput} setFileInput={setFileInput} uploadStatus={uploadStatus} setUploadStatus={setUploadStatus} uploadedFileData={uploadedFileData} setUploadedFileData={setUploadedFileData}/>
+            <Button type="button" variant="primary" mb="1.6rem" disabled={isButtonDisabled} onClick={() => {handleCreateProject()}}>Criar projeto</Button>
             <Button type="button" variant="secondary" onClick={() => {closePopup(false)}}>Cancelar</Button>
         </StyledCreateProjectPopup>
     )
