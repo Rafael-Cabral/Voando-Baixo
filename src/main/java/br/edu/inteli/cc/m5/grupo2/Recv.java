f0c3de4be1a9f58f472bc03c3c7b20d40e467701
Elias Biondo <64558682+eliasbiondo@users.noreply.github.com>
2023-03-22 17:32:21 -0300

    feat: generating graph from project id

--- src/main/java/br/edu/inteli/cc/m5/grupo2/Recv.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/Recv.java
@@ -49,6 +49,9 @@ public class Recv {
                     System.out.println(" Destination ------------------------------------------------------------");
                     System.out.println("     Latitude: " + destinationLatitude);
                     System.out.println("     Longitude: " + destinationLongitude);
+
+                    Graph graph = createGraph(projectId);
+
                     System.out.println("\n Route found for project '" + projectId + "'.");
                     System.out.println("\n===============================================================================================\n");
 
@@ -96,50 +99,65 @@ public class Recv {
 
 
     private Connection createConnection() throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, TimeoutException {
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUri(URI);
         return factory.newConnection();
+
     }
 
     private Channel createChannel(Connection connection) throws IOException {
+
         return connection.createChannel();
+
     }
 
     private void declareQueue(Channel channel, String queueName) throws IOException {
+
         channel.queueDeclare(queueName, true, false, false, null);
         System.out.println(" Waiting for new messages in queue '" + queueName + "'.");
+
     }
 
     private void processMessage(String projectId) throws Exception {
+
         Graph graph = createGraph(projectId);
         Neo4j neo4j = new Neo4j();
         neo4j.persistMapBounds(graph, getRows(projectId), projectId);
+
     }
 
     private Graph createGraph(String projectId) throws Exception {
+
         String path = System.getProperty("user.dir") + "/downloads/" + projectId + ".dt2";
         double[][] map = Dted.readDted(path, 180);
+
         System.out.println(" File read successfully.");
-        System.out.println(" Loading graph instance");
+        System.out.println(" Loading graph instance.");
+
         Graph graph = new Graph();
         for (int i = 0; i < map.length - 1; i++) {
             graph.addVertex(map[i][1], map[i][2], map[i][0]);
         }
+
         System.out.println(" Graph loaded successfully.");
+        System.out.println(" Connecting graph vertices...");
 
         int rows = (int) map[map.length - 1][1];
         int cols = (int) map[map.length - 1][2];
-
-        System.out.println(" Connecting graph vertices...");
         graph.connectVertices(180, rows, cols);
+
         System.out.println(" Graph vertices connected successfully.");
 
         return graph;
+
     }
 
     private int getRows(String projectId) throws Exception {
+
         String path = System.getProperty("user.dir") + "/downloads/" + projectId + ".dt2";
         double[][] map = Dted.readDted(path, 180);
         return (int) map[map.length - 1][1];
+
     }
 }
