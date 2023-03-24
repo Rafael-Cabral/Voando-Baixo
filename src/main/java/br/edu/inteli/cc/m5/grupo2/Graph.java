b785659e59db1f285457d79069260e1ebc634a68
ViniciusSouza25 <99210113+ViniciusSouza25@users.noreply.github.com>
2023-03-24 15:27:44 -0300

    Revert "Update Graph.java"
    This reverts commit 429a6e9c9c5abdfa15a1a13dd9bd710a80534877.

--- src/main/java/br/edu/inteli/cc/m5/grupo2/Graph.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo2/Graph.java
@@ -4,72 +4,54 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//start of the graph backend
+
 public class Graph {
-// declares a private static final Logger object named "LOGGER", which is used to log messages in the Graph class.
-private static final Logger LOGGER = Logger.getLogger(Graph.class.getName());
-// declares a private final instance variable named "vertices", which is an ArrayList of Vertex objects.
-private final ArrayList<Vertex> vertices;
-// declares a private instance variable named "nextVertexId", which is an integer that represents the next available ID for a vertex.
-private int nextVertexId = 0;
-// declares private instance variables named "cols" and "rows", which represent the number of columns and rows in a grid of vertices, respectively.
-private int cols, rows;
 
-public Graph() {
-    // initializes the "vertices" instance variable with an empty ArrayList.
-    this.vertices = new ArrayList<>();
-}
+    private static final Logger LOGGER = Logger.getLogger(Graph.class.getName());
+    private final ArrayList<Vertex> vertices;
+    private int nextVertexId = 0;
 
-// declares a public method named "addVertex", which adds a new Vertex object to the "vertices" ArrayList.
-public void addVertex(double latitude, double longitude, double altitude) {
-    // creates a new Vertex object with the specified latitude, longitude, altitude, and the next available ID, and assigns it to a local variable named "vertex".
-    Vertex vertex = new Vertex(nextVertexId++, latitude, longitude, altitude);
-    // adds the new Vertex object to the "vertices" ArrayList.
-    this.vertices.add(vertex);
-}
+    private int cols, rows;
 
-// declares a public method named "getCols", which returns the number of columns in the grid.
-public int getCols(){return this.cols;}
-
-// declares a public method named "getRows", which returns the number of rows in the grid.
-public int getRows(){return this.rows;}
-
-// Remove before deploy
-// declares a public method named "addVertex", which adds a new Vertex object to the "vertices" ArrayList.
-public void addVertex(Vertex vertex){
-    // begins a for loop that iterates through the "vertices" ArrayList.
-    for (int i = 0; i < this.getVertices().size(); i++){
-        // checks if the ID of the new Vertex object matches the ID of any existing Vertex objects in the "vertices" ArrayList.
-        if (vertex.getId() == this.getVertices().get(i).getId()){
-            // logs a warning message indicating that a Vertex object with the same ID already exists in the "vertices"
-            LOGGER.log(Level.WARNING, "Vertex with id == " + vertex.getId() + " already exists");
-            return;
-        }
+    public Graph() {
+        this.vertices = new ArrayList<>();
     }
-    // adds the new Vertex object to the "vertices" ArrayList.
-    this.vertices.add(vertex);
-}
 
-public void addEdge(int vertexId, int arrivalVertexId) {
-    // adds a connection between the Vertex object with ID "vertexId" and the Vertex object with ID "arrivalVertexId".
-    this.vertices.get(vertexId).addConnectionTo(this.vertices.get(arrivalVertexId));
-}
+    public void addVertex(double latitude, double longitude, double altitude) {
+        Vertex vertex = new Vertex(nextVertexId++, latitude, longitude, altitude);
+        this.vertices.add(vertex);
+    }
 
-public ArrayList<Vertex> getVertices() {
-    // returns the "vertices" ArrayList.
-    return this.vertices;
-}
+    public int getCols(){return this.cols;}
 
-public LinkedList<Edge> getConnectionsOf(int vertexId) {
-    // returns a LinkedList of Edge objects representing all the connections of the Vertex object with ID "vertexId".
-    return this.vertices.get(vertexId).getAllConnections();
-}
+    public int getRows(){return this.rows;}
 
-public String getAdjacencyListForEachVertex(){
-    StringBuilder str = new StringBuilder();
-    // begins a for loop that iterates through the "vertices" ArrayList.
-    for (Vertex vertex : this.vertices) {
-        // appends the adjacency list of each Vertex object to the StringBuilder object "
+    // Remove before deploy
+    public void addVertex(Vertex vertex){
+        for (int i = 0; i < this.getVertices().size(); i++){
+            if (vertex.getId() == this.getVertices().get(i).getId()){
+                LOGGER.log(Level.WARNING, "Vertex with id == " + vertex.getId() + " already exists");
+                return;
+            }
+        }
+        this.vertices.add(vertex);
+    }
+
+    public void addEdge(int vertexId, int arrivalVertexId) {
+        this.vertices.get(vertexId).addConnectionTo(this.vertices.get(arrivalVertexId));
+    }
+
+    public ArrayList<Vertex> getVertices() {
+        return this.vertices;
+    }
+
+    public LinkedList<Edge> getConnectionsOf(int vertexId) {
+        return this.vertices.get(vertexId).getAllConnections();
+    }
+
+    public String getAdjacencyListForEachVertex(){
+        StringBuilder str = new StringBuilder();
+        for (Vertex vertex : this.vertices) {
             str.append(vertex.getAdjacencyListAsString());
             str.append("\n");
         }
@@ -136,5 +118,38 @@ public String getAdjacencyListForEachVertex(){
                 currentVertex++;
             }
         }
+    }
+    public Vertex findNearestVertex(double targetLatitude, double targetLongitude) {
+        if (vertices.isEmpty()) {
+            return null;
+        }
+
+        Vertex nearestVertex = vertices.get(0);
+        double minDistance = distance(nearestVertex.getLatitude(), nearestVertex.getLongitude(), targetLatitude, targetLongitude);
+
+        for (Vertex vertex : vertices) {
+            double currentDistance = distance(vertex.getLatitude(), vertex.getLongitude(), targetLatitude, targetLongitude);
+
+            if (currentDistance < minDistance) {
+                nearestVertex = vertex;
+                minDistance = currentDistance;
+            }
+        }
+
+        return nearestVertex;
+    }
+
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = 6371 * c; // Radius of the earth in km
+
+        return distance;
     }
 }
